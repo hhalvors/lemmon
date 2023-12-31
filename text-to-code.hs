7ef8e0d711e19d2ba1da4c93d93c2be2b6a0c9a6
hhalvors <hhalvors@princeton.edu>
2023-12-31 11:05:40 +0100

    updated parsers

--- text-to-code.hs
+++ text-to-code.hs
@@ -1,6 +1,10 @@
 import System.IO
 import System.FilePath
 import Text.Regex.TDFA
+import Text.Parsec
+import Text.Parsec.String (Parser)
+import qualified Data.Set as Set
+import Control.Applicative ((<|>))
 
 -- Assuming EatProofLine is defined somewhere in your code
 eatProofLine :: String -> String  -- Replace with the actual type signature
@@ -17,3 +21,60 @@ processFile filePath = do
         _ -> putStrLn "No matching line found in the file."
   where
     pattern = "^[0-9]+(,[0-9]+)* \\([0-9]+\\)"
+
+-- Parser for an integer
+integer :: Parser Int
+integer = read <$> many1 digit
+
+-- Parser for a comma
+comma :: Parser Char
+comma = char ','
+
+-- Parser to get dependencies (list of integers separated by commas)
+getDependencies :: Parser (Set.Set Int)
+getDependencies = Set.fromList <$> integer `sepBy` comma
+
+-- Assuming you have two other parsers
+-- otherParser1 :: Parser <Type1>
+-- otherParser2 :: Parser <Type2>
+
+-- Main parser that uses getDependencies and other sub-parsers
+eatProofLine :: Parser <YourDataType>
+eatProofLine = do
+    deps <- getDependencies
+    -- Parse other parts of the line using otherParser1 and otherParser2
+    -- Combine the results into YourDataType
+    -- return <combined result>
+
+-- Example of how to run the parser
+parseProofLine :: String -> Either ParseError <YourDataType>
+parseProofLine input = parse eatProofLine "" input
+
+-- Parser for an integer
+integer :: Parser Int
+integer = read <$> many1 digit
+
+-- Parser for a comma
+comma :: Parser Char
+comma = char ','
+
+-- Parser for a space
+spaceParser :: Parser Char
+spaceParser = char ' '
+
+-- Parser for "MP"
+mpParser :: Parser String
+mpParser = string "MP"
+
+-- Parser for Justification
+getJustification :: Parser Justification
+getJustification =
+  try (string "A" >> return Assumption)
+  <|> (do
+        m <- integer
+        _ <- comma
+        n <- integer
+        _ <- spaceParser
+        _ <- mpParser
+        return (ModusPonens m n))
+
